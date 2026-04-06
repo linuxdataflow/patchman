4a88d52ceef955a7472f467d641bf082e5969137
Pierig Le Saux <pierig56@gmail.com>
2026-04-05 20:30:30 -0400

    update

---
+++ reports/migrations/0007_merge_0006_report_installed_packages_0006_report_phased_deferred_updates.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('reports', '0006_report_installed_packages'),
+        ('reports', '0006_report_phased_deferred_updates'),
+    ]
+
+    operations = []
